2308ba77f5275d21bf652d85149e7037891bad92
varshini39 <ragavs95@gmail.com>
2023-10-24 22:44:32 +0530

    Adding entities needed for all services

---
+++ src/main/java/com/app/project/EntityServiceApplication.java
@@ -0,0 +1,16 @@
+package com.app.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityServiceApplication {
+	
+	// Need to add this project as local maven project for entity reference
+	// mvn clean install
+	// mvn install:install-file -Dfile=./target/entity-service-1.0.0.jar -DgroupId=com.app.project -DartifactId=entity-service -Dversion=1.0.0 -Dpackaging=jar
+	public static void main(String[] args) {
+		SpringApplication.run(EntityServiceApplication.class, args);
+	}
+	
+}
